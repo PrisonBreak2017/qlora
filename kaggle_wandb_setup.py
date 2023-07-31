31d2b9188439fc734080a39d50c91f0b12ccb0af
wdjrpg <wdjrpg@163.com>
2023-07-31 20:53:14 +0800

    wandb init

--- kaggle_wandb_setup.py
+++ kaggle_wandb_setup.py
@@ -1,5 +1,13 @@
 #hard code for kaggle project
 import os
+
+
 os.environ["WANDB_PROJECT"] = "qlora_on_kaggle"
 os.environ["WANDB_WATCH"] = "all"
 os.environ["WANDB_LOG_MODEL"] = "true"
+
+import wandb
+wandb.init(project=os.environ["WANDB_PROJECT"])
+wandb.watch(os.environ["WANDB_WATCH"] )
+wandb.log_model = os.environ["WANDB_LOG_MODEL"]
+
