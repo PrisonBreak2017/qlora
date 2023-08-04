e5062ba7b24ca7faf740581964b47e489c141b0f
PrisonBreak2017 <wdjrpg@hotmail.com>
2023-08-04 23:29:17 +0800

    upload the full model

---
+++ upload_model.py
@@ -0,0 +1,40 @@
+import os
+import torch
+from datasets import load_dataset
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    HfArgumentParser,
+    TrainingArguments,
+    pipeline,
+    logging,
+)
+
+from peft import LoraConfig, PeftModel
+
+# Load the entire model on the GPU 0
+device_map = {"": 0}
+
+model_name = "huggyllama/llama-7b"
+new_model = "output/guanaco7b/checkpoint-187/adapter_model"
+
+# Reload model in FP16 and merge it with LoRA weights
+base_model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    low_cpu_mem_usage=True,
+    return_dict=True,
+    torch_dtype=torch.float16,
+    device_map=device_map,
+)
+model = PeftModel.from_pretrained(base_model, new_model)
+model = model.merge_and_unload()
+
+# Reload tokenizer to save it
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = "right"
+
+model.push_to_hub(new_model, use_temp_dir=False)
+tokenizer.push_to_hub(new_model, use_temp_dir=False)
+
